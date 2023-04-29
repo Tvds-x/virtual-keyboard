a1d77bdaee7912d013ab4f72812199138cfb694a
Tvds-x <tvds.x@yandex.ru>
2023-04-29 16:34:09 +0300

    feat: add textarea, textarea styles and  description

--- src/app.js
+++ src/app.js
@@ -13,29 +13,56 @@ function createKeyboard(initLang) {
     if (additionalClasses.includes(element.keyCode)) {
       key.classList.add(`${element.keyCode.charAt(0).toLowerCase() + element.keyCode.slice(1)}`);
       key.innerText = element.small;
+      key.setAttribute('data', element.keyCode);
       keyboard.appendChild(key);
     } else if (additionalClassesToArrows.includes(element.keyCode)) {
       key.classList.add(`${element.keyCode.charAt(0).toLowerCase() + element.keyCode.slice(1)}`);
       key.innerText = element.small;
+      key.setAttribute('data', element.keyCode);
       arrows.appendChild(key);
     } else {
       key.innerText = element.small;
+      key.setAttribute('data', element.keyCode);
       keyboard.appendChild(key);
     }
     keyboard.appendChild(arrows);
   });
   return keyboard;
 }
-// const keyHandler = (event) => {
-//   document.querySelectorAll('.keys').forEach()
-//   console.log(event);
-// };
 
+const keyDownHandler = (event) => {
+  event.preventDefault();
+  document.querySelector(`.key[data=${event.code}]`).classList.add('active');
+};
+const keyUpHandler = (event) => {
+  document.querySelector(`.key[data=${event.code}]`).classList.remove('active');
+};
+
+function createDescription() {
+  const description = document.createElement('div');
+  description.classList.add('description');
+  const systemDescription = document.createElement('p');
+  systemDescription.innerText = 'Created in Ubuntu WSL';
+  systemDescription.classList.add('system-desc');
+  const langSwitchDescription = document.createElement('p');
+  langSwitchDescription.innerText = 'Language switch: LShift + Alt';
+  langSwitchDescription.classList.add('lang-desc');
+
+  description.append(systemDescription, langSwitchDescription);
+  return description;
+}
+
+function createTextArea() {
+  const textArea = document.createElement('textarea');
+  textArea.classList.add('input');
+  textArea.setAttribute('cols', 145);
+  textArea.setAttribute('rows', 7);
+  return textArea;
+}
+
+document.body.appendChild(createDescription());
+document.body.appendChild(createTextArea());
 document.body.appendChild(createKeyboard(lang.ru));
 
-// document.body.onkeydown = keyHandler;
-
-// const keys = document.querySelectorAll('.key');
-// keys.forEach((key) => {
-//   key.addEventListener('click', keyHandler);
-// });
+document.body.addEventListener('keydown', keyDownHandler);
+document.body.addEventListener('keyup', keyUpHandler);
