79ef65476d30a30422d484fda83e80fa89da6f26
Tvds-x <tvds.x@yandex.ru>
2023-04-29 18:07:29 +0300

    feat: lang switch, keys validate

--- src/app.js
+++ src/app.js
@@ -2,18 +2,71 @@ import lang from './lang/lang.js';
 import createDescription from './modules/description.js';
 import createTextArea from './modules/textarea.js';
 import createKeyboard from './modules/keyboard.js';
+import ru from './lang/ru.js';
+
+function init() {
+  document.body.appendChild(createDescription());
+  document.body.appendChild(createTextArea());
+  if (localStorage.getItem('language')) {
+    if (localStorage.getItem('language') === 'ru') {
+      document.body.appendChild(createKeyboard(lang.ru));
+    } else {
+      document.body.appendChild(createKeyboard(lang.eng));
+    }
+  } else {
+    document.body.appendChild(createKeyboard(lang.ru));
+    localStorage.setItem('language', 'ru');
+  }
+  // eslint-disable-next-line no-use-before-define
+  document.body.addEventListener('keydown', keyDownHandler);
+  // eslint-disable-next-line no-use-before-define
+  document.body.addEventListener('keyup', keyUpHandler);
+}
+
+let altFlag = false;
+
+function isValidKey(event) {
+  let flag = false;
+  ru.forEach((key) => {
+    if (event.code === key.keyCode) flag = true;
+  });
+  if (flag) {
+    return true;
+  }
+  return false;
+}
 
 const keyDownHandler = (event) => {
-  // event.preventDefault();
-  document.querySelector(`.key[data=${event.code}]`).classList.add('active');
+  if (isValidKey(event)) {
+    event.preventDefault();
+    document.querySelector(`.key[data=${event.code}]`).classList.add('active');
+    if (event.code === 'AltLeft') altFlag = true;
+    if (event.code === 'ShiftLeft' && altFlag) {
+      altFlag = false;
+      if (localStorage.getItem('language') === 'eng') {
+        document.body.lastChild.replaceWith(createKeyboard(lang.ru));
+        // eslint-disable-next-line no-use-before-define
+        addListeners();
+        localStorage.setItem('language', 'ru');
+      } else {
+        document.body.lastChild.replaceWith(createKeyboard(lang.eng));
+        // eslint-disable-next-line no-use-before-define
+        addListeners();
+        localStorage.setItem('language', 'eng');
+      }
+    }
+  }
 };
+
 const keyUpHandler = (event) => {
-  document.querySelector(`.key[data=${event.code}]`).classList.remove('active');
+  if (isValidKey(event)) {
+    document.querySelector(`.key[data=${event.code}]`).classList.remove('active');
+  }
 };
 
-document.body.appendChild(createDescription());
-document.body.appendChild(createTextArea());
-document.body.appendChild(createKeyboard(lang.ru));
+function addListeners() {
+  document.body.addEventListener('keydown', keyDownHandler);
+  document.body.addEventListener('keyup', keyUpHandler);
+}
 
-document.body.addEventListener('keydown', keyDownHandler);
-document.body.addEventListener('keyup', keyUpHandler);
+init();
